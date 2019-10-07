3a5ffd2ef2b242047aa688a806b7fbfe91bd96dc
Jonas Alves <joonasjss@gmail.com>
2019-10-06 23:38:18 -0300

    feat: 🎸 Dashboard and booking finalized

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ const UploadConfig = require("./config/upload");
 
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
 const routes = express.Router();
 const upload = multer(UploadConfig);
@@ -11,5 +13,10 @@ const upload = multer(UploadConfig);
 routes.post("/sessions", SessionController.store);
 
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+routes.get("/spots", SpotController.index);
+
+routes.get("/dashboard", DashboardController.show);
+
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
