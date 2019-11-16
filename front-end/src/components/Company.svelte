4fbe9cc064e3da2d0c3b699c2b7c7f825eea09ed
Jonas Alves <joonasjss@gmail.com>
2019-11-16 20:41:10 -0300

    feat: 🎸 Company component started

---
+++ front-end/src/components/Company.svelte
@@ -0,0 +1,21 @@
+<script>
+  //   export let title, className
+</script>
+
+<style lang="scss">
+  .container {
+    margin: 10px;
+    width: 100%;
+
+    img {
+      width: 100%;
+    }
+  }
+</style>
+
+<div class="container">
+  <img
+    src="https://peakon.com/wp-content/uploads/2018/06/google-company-culture-5-1120x575.jpg" />
+  <h2>Google</h2>
+  <h3>Free</h3>
+</div>
--- front-end/src/routes/listCompanies.svelte
+++ front-end/src/routes/listCompanies.svelte
@@ -4,6 +4,9 @@
 
   import axios from 'axios'
 
+  // Component
+  import Company from '../components/Company.svelte'
+
   onMount(async () => {
     if (!localStorage.getItem('logged')) {
       goto('/')
@@ -12,21 +15,33 @@
 </script>
 
 <style lang="scss">
-  .home {
+  .list-companies {
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     color: $black;
+    .content {
+      max-width: 420px;
+      display: flex;
+      width: 100%;
+      padding: 22px;
+      background: #fff;
+      margin-top: 50px;
+      border-radius: 4px;
+    }
     img {
       margin-bottom: 56px;
     }
   }
 </style>
 
-<div class="home">
+<div class="list-companies">
 
   <img src="images/logo.svg" alt="Aircnc" />
-
+  <div class="content">
+    <Company />
+    <Company />
+  </div>
 </div>
